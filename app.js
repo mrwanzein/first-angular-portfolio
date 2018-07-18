8991e59eedf229486526c5a8e11dd224948877f5
Mrwan Zein <mrwanzein@outlook.com>
2018-07-18 16:46:05 -0400

    fixed email function

--- app.js
+++ app.js
@@ -39,7 +39,9 @@ app.post('/client-contact', (req, res) => {
       html: clientMsg,
     };
     
-    sgMail.send(msg);
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    res.send(sgMail.send(msg));
     
 });
 
